705845fe24ae9e220d034c08ef83fb22e34edb9f
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-12-07 10:05:54 +0100

    feat: Add form validation to Signup component
    This update introduces comprehensive form validation in the Signup component, ensuring data integrity and enhancing user experience. Key validations include email format, password length, matching passwords, mandatory fields, role selection, and acceptance of terms. Error messages are displayed to guide users in correcting form input mistakes.

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,8 +1,75 @@
-import React from "react";
+import React, { useActionState } from "react";
+import {
+  hasMinLength,
+  isEmail,
+  isEqualsToOtherValue,
+  isNotEmpty,
+} from "../util/validation.ts";
 
 const Signup: React.FC = () => {
+  // prevFormState has to be accepted within form action (initially it is the same as initialState we passed to useActionState)
+  const signupAction = (
+    prevFormState: { errors: string[] | null },
+    formData: FormData,
+  ) => {
+    const email = formData.get("email") as string;
+    const password = formData.get("password") as string;
+    const confirmPassword = formData.get("confirm-password") as string;
+    const firstName = formData.get("first-name") as string;
+    const lastName = formData.get("last-name") as string;
+    const role = formData.get("role") as string;
+    const terms = formData.get("terms") as string;
+    const acquisition = formData.getAll("acquisition") as string[]; // "getAll" returns an array
+
+    const errors = [];
+
+    if (isNotEmpty(email) && !isEmail(email)) {
+      errors.push("Email is not valid");
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push("Password must be at least six characters");
+      errors.push("You must provide a password with at least six characters");
+    }
+
+    if (!isEqualsToOtherValue(password, confirmPassword)) {
+      errors.push("Passwords must match");
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push("First and last name must be provided");
+    }
+
+    if (!role) {
+      errors.push("You must select a role");
+    }
+
+    if (!terms) {
+      errors.push("You must accept the terms and conditions");
+    }
+
+    if (acquisition.length === 0) {
+      errors.push("You must select at least one acquisition channel");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
+    console.log({ prevFormState });
+
+    return {
+      errors: null,
+    };
+  };
+
+  const [formState, formAction] = useActionState(signupAction, {
+    errors: null,
+  });
+
+  console.log({ formState });
+
   return (
-    <form>
+    <form action={formAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
@@ -86,6 +153,15 @@ const Signup: React.FC = () => {
           agree to the terms and conditions
         </label>
       </div>
+      {formState.errors && (
+        <ul className="errors">
+          {formState.errors?.map((error) => (
+            <li className="error" key={error}>
+              {error}
+            </li>
+          ))}
+        </ul>
+      )}
 
       <p className="form-actions">
         <button type="reset" className="button button-flat">
